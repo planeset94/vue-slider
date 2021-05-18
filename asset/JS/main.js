c946ecb591141f851a4032d3d9eb9ea8aa786f1d
planeset94 <planeset94@gmail.com>
2021-05-18 18:49:05 +0200

    Managed ArrowLeft/Right

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -6,7 +6,8 @@ const app = new Vue({
             'https://scitechdaily.com/images/Mars-Red-Planet-Highlights-scaled.jpg',
             'https://media.newyorker.com/photos/60994dabccd28612d3013cec/master/pass/Mann-SpaceEthics.jpg',
             'https://cdn.britannica.com/24/73524-050-3C2F3432/result-seasons-Mars-planet-inclination-plane-cap.jpg',
-            'https://cdn.mos.cms.futurecdn.net/iWG5zg5Mpt8AvZqc4vYa4K.jpg'
+            'https://cdn.mos.cms.futurecdn.net/iWG5zg5Mpt8AvZqc4vYa4K.jpg',
+
         ],
 
 
@@ -45,22 +46,27 @@ const app = new Vue({
 
 
 
+
+
+
+
     },
-    // mounted() {
-    //     alert('Hello')
-    // }
+    mounted() {
+        document.addEventListener('keydown', (e) => {
+            const key = e.key;
+            if (key == 'ArrowLeft') {
+                console.log('sinistra');
+                app.prev();
+            } else if (key == 'ArrowRight') {
+                app.next();
+                console.log('destra');
+
+            }
+        });
+    }
 });
 
-//Verifica quale tasto è stato cliccato e chiede alla pagina di muovere le img a destra o sinistra
-function verificaTasto(e) {
-    if (e.keyCode = '37') {
-        app.prev();
-    } else if (e.keyCode = '39') {
-        app.next();
 
-    }
 
-};
 
-document.addEventListener('keyup', verificaTasto);
 
